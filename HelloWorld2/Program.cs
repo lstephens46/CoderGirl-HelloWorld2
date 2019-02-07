276f8a88dbcfdc6290222e572f1ce0c4812e99e1
Lauren Stephens <9128505+lstephens46@users.noreply.github.com>
2019-02-06 19:39:22 -0600

    finish HelloWorld2.cs and run debug tests

--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -9,10 +9,12 @@ namespace HelloWorld2
             Console.Write("Please enter your name: ");
 
             // Store a name the user enters to a variable.
+            string name = Console.ReadLine();
 
-            // Output - Hello (name)!
-
+            // Output - Hello name
+            Console.WriteLine($"Hello {name}!");
             Console.ReadLine();
+
         }
     }
 }
